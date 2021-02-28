9871e1fef2f674c5f1c1371676da9667a7cb5549
mattiapomelli <mattiapomelli@gmail.com>
2021-02-28 22:31:57 +0100

    landing page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,69 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import ProjectCard from '../components/ProjectCard'
+import BlogPostPreview from '../components/BlogPostPreview'
+import { getSortedPosts } from '../lib/mdx'
 
-export default function Home() {
+const projects = [
+  {
+    title: 'Codeshare',
+    description:
+      'A platform where programmers can share code snippets and find solutions to coding problems',
+    tags: ['Next.js', 'Styled-components', 'GraphQL', 'PostgreSQL', 'Hasura'],
+    link: 'https://codeshare.tech/',
+    githubLink: 'https://github.com/mattiapomelli/codeshare',
+  },
+  {
+    title: 'Countrip',
+    description:
+      'An interactive map where you can see data and statistics about world countries',
+    tags: ['React', 'REST Countries API', 'Leaflet Maps'],
+    link: 'https://countrip.netlify.app/',
+    githubLink: 'https://github.com/mattiapomelli/countrip',
+  },
+]
+
+export default function Home({ latestPosts }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <h1 className="text-6xl font-extrabold mb-4 mt-8 tracking-tight">
+        Hey, I'm Mattia
+      </h1>
+      <p className="text-lg mb-16 ml-0.5 text-dark-gray dark:text-gray-300">
+        I'm a front end web developer focused on using modern and innovative
+        technologies to bring ideas from my mind to the web. I also love to make
+        videos and write piano songs.
+      </p>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h3 className="text-5xl font-extrabold mb-3 mt-8 tracking-tight">
+        Projects
+      </h3>
+      <p className="text-lg ml-0.5 text-dark-gray dark:text-gray-300">
+        Some of the last projects I've been working on
+      </p>
+      <div className="grid grid-cols-autofill gap-4 mt-6 mb-20">
+        {projects.map((project) => (
+          <ProjectCard key={project.title} {...project} />
+        ))}
+      </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <h3 className="text-5xl font-extrabold mb-3 mt-8 tracking-tight">
+        Articles
+      </h3>
+      <p className="text-lg ml-0.5 mb-8 text-dark-gray dark:text-gray-300">
+        Some articles that I've written about coding and technology
+      </p>
+      {latestPosts.map((post) => (
+        <BlogPostPreview key={post.slug} {...post} />
+      ))}
+    </>
   )
+}
+
+export async function getStaticProps() {
+  const posts = getSortedPosts()
+
+  return {
+    props: {
+      latestPosts: posts.slice(0, 3),
+    },
+  }
 }
