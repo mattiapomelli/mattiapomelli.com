20b1e701d0f19d095fc152d21099b3237c317c87
mattiapomelli <mattiapomelli@gmail.com>
2021-02-28 22:11:56 +0100

    added mdx

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 
+import { MDXProvider } from '@mdx-js/react'
+
+import MDXComponents from '../components/MDXComponents'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MDXProvider components={MDXComponents}>
+      <Component {...pageProps} />
+    </MDXProvider>
+  )
 }
 
 export default MyApp
