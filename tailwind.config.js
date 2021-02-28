13cc19b75474cd8a4c97d54d87ad7755102b6171
mattiapomelli <mattiapomelli@gmail.com>
2021-02-28 21:56:48 +0100

    added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,78 @@
+module.exports = {
+  purge: ['./pages/**/*.js', './components/**/*.js', './layouts/**/*.js'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        main: ['Nunito Sans', 'sans-serif'],
+        secondary: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'main-blue': '#086DFF',
+        'main-light-blue': '#72CAF3',
+        'light-blue-gray': '#EDF2F7',
+        'main-black': '#343A40',
+        'dark-gray': '#575757',
+        'dark-blue-600': '#151f28',
+        'dark-blue-800': '#0E141B',
+      },
+      gridTemplateColumns: {
+        autofill: 'repeat(auto-fill, minmax(300px, 1fr))',
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.700'),
+            a: {
+              // links color
+              color: theme('colors.blue.500'),
+              '&:hover': {
+                color: theme('colors.blue.700'),
+              },
+            },
+            'code::before': false, // hide backticks from inline code
+            'code::after': false,
+            'blockquote p:first-of-type::before': false, // remove quotation marks in blockquotes
+            'blockquote p:last-of-type::after': false,
+          },
+        },
+        dark: {
+          css: {
+            color: theme('colors.gray.300'),
+            a: {
+              color: theme('colors.blue.400'),
+              '&:hover': {
+                color: theme('colors.blue.600'),
+              },
+            },
+            blockquote: {
+              borderLeftColor: theme('colors.gray.600'),
+              color: theme('colors.gray.300'),
+            },
+            'h2,h3,h4,strong': {
+              color: 'white',
+            },
+            hr: { borderColor: theme('colors.gray.700') },
+            ol: {
+              // list dots color
+              li: {
+                '&:before': { color: theme('colors.gray.500') },
+              },
+            },
+            ul: {
+              li: {
+                '&:before': { backgroundColor: theme('colors.gray.500') },
+              },
+            },
+          },
+        },
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      typography: ['dark'],
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
