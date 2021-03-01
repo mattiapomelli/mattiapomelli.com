a01e50c352fc3f6e6a00bad6ae4460bb31ebe5fb
mattiapomelli <mattiapomelli@gmail.com>
2021-03-01 14:22:41 +0100

    fix - awaiting views update and error handling

--- pages/api/views/[slug].js
+++ pages/api/views/[slug].js
@@ -2,24 +2,45 @@ import { db, increment } from '../../../lib/firebase'
 
 export default async (req, res) => {
   if (req.method === 'PUT') {
-    const docRef = await db.collection('views').doc(req.query.slug)
+    try {
+      const docRef = db.collection('views').doc(req.query.slug)
 
-    docRef.set({ views: increment }, { merge: true })
+      const doc = await docRef.get()
 
-    return res.status(200).json({
-      message: 'Views updated',
-    })
-  } else if (req.method === 'GET') {
-    const doc = await db.collection('views').doc(req.query.slug).get()
+      if (doc.exists) {
+        await docRef.update({ views: increment })
+      } else {
+        await docRef.set({ views: 1 })
+      }
 
-    if (!doc.exists)
-      return res.status(404).json({
-        message: 'Document not found',
+      return res.status(200).json({
+        message: 'Views updated',
       })
+    } catch (err) {
+      return res.status(500).json({
+        error: err.message || 'Something went wrong',
+      })
+    }
+  } else if (req.method === 'GET') {
+    try {
+      const doc = await db.collection('views').doc(req.query.slug).get()
 
-    return res.status(200).json({
-      total: doc.data().views || 0,
-    })
+      if (!doc.exists) {
+        return res.status(404).json({
+          message: 'Document not found',
+        })
+      }
+
+      const views = doc.data().views || 0
+
+      return res.status(200).json({
+        total: views,
+      })
+    } catch (err) {
+      return res.status(500).json({
+        error: err.message || 'Something went wrong',
+      })
+    }
   } else {
     res.status(400).json({
       message: 'Method not allowed',
