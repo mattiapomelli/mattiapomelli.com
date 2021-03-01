33509d307da85ee9d2aaa3dcf75a761dd8b77c22
mattiapomelli <mattiapomelli@gmail.com>
2021-03-01 21:24:01 +0100

    self hosting fonts

--- pages/_document.js
+++ pages/_document.js
@@ -4,17 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
