89ed6fbf812761dbdf2d33bea0c98dd15a75c41d
Mattia Pomelli <mattiapomelli@gmail.com>
2022-08-28 14:56:24 +0200

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    providerImportSource: '@mdx-js/react',
+  },
 })
 
 module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'mdx'],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require('./scripts/generate-sitemap')
-    }
+  ...nextConfig,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+    })
 
     return config
   },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 })
